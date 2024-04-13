75f97a679644ebb98ac9359178f0818e662f51dd
root <root@EL-PC>
2024-04-13 13:12:56 -0300

    setting the database url

--- ext/configuration.py
+++ ext/configuration.py
@@ -4,5 +4,5 @@ import os
 def init_app(app):
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
     app.config['PERMANENT_SESSION_LIFETIME'] = int(os.environ.get('PERMANENT_SESSION_LIFETIME'))
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('MYSQL_URL')
 
