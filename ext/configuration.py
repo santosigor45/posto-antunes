dfe9240ad6deb9c9dfe7d2d06f5c09193db7852d
santo <santosigor45@gmail.com>
2024-04-13 17:33:35 -0300

    updating requirements.

--- ext/configuration.py
+++ ext/configuration.py
@@ -4,5 +4,5 @@ import os
 def init_app(app):
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
     app.config['PERMANENT_SESSION_LIFETIME'] = int(os.environ.get('PERMANENT_SESSION_LIFETIME'))
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('MYSQL_PRIVATE_URL')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('MYSQL_URL')
 
