0040d5898fbbb8091621c74e6b40a5a55bc71a29
santo <santosigor45@gmail.com>
2024-04-18 14:56:51 -0300

    including search and filter tools in the admin template.

--- blueprints/admin/__init__.py
+++ blueprints/admin/__init__.py
@@ -1,5 +1,5 @@
 from flask_admin import Admin
-from .admin_views import MyModelView, MyAdminIndexView
+from .admin_views import *
 from ext.database import db
 from models import Abastecimentos, EntregaCombustivel, Placas, Motoristas, Cidades, Postos, PontoVirada, VolumeAtual
 
@@ -7,10 +7,10 @@ from models import Abastecimentos, EntregaCombustivel, Placas, Motoristas, Cidad
 def init_app(app):
     admin = Admin(app, template_mode='bootstrap3', base_template='admin_template.html', url='/admin', index_view=MyAdminIndexView())
 
-    admin.add_view(MyModelView(Abastecimentos, db.session, category='Lançamentos'))
-    admin.add_view(MyModelView(EntregaCombustivel, db.session, category='Lançamentos'))
-    admin.add_view(MyModelView(Placas, db.session, category='Dados'))
-    admin.add_view(MyModelView(Motoristas, db.session, category='Dados'))
+    admin.add_view(AbastecimentosView(Abastecimentos, db.session, category='Lançamentos'))
+    admin.add_view(EntregaCombustivelView(EntregaCombustivel, db.session, category='Lançamentos'))
+    admin.add_view(PlacasView(Placas, db.session, category='Dados'))
+    admin.add_view(MotoristasView(Motoristas, db.session, category='Dados'))
     admin.add_view(MyModelView(Cidades, db.session, category='Dados'))
     admin.add_view(MyModelView(Postos, db.session, category='Dados'))
     admin.add_view(MyModelView(PontoVirada, db.session, category='Dados'))
