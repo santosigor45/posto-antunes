6f26498fea9e01fa4f29cefdf9bb68efa5d57a2a
santosigor45 <santosigor45@github.com>
2024-06-17 20:03:26 -0300

    correcting a typo in ext/configuration.py file

--- ext/configuration.py
+++ ext/configuration.py
@@ -3,7 +3,6 @@ import os
 
 # Fetch environment variables to configure the application settings
 def init_app(app):
-    app.url_map.default_subdomain = "posto"
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
     app.config['PERMANENT_SESSION_LIFETIME'] = int(os.environ.get('PERMANENT_SESSION_LIFETIME'))
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('MYSQL_URL')
